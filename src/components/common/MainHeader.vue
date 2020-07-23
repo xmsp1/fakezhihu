242a4496a0df00f9e992a2a4e79c06a1a786f870
xmsp1 <1987324004@qq.com>
2020-07-23 09:53:16 +0800

    测试远程

---
+++ src/components/common/MainHeader.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <header>
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
+  </header>
+</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainHeader></MainHeader>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MainHeader from 'components/common/MainHeader'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    MainHeader
   }
 }
 </script>
